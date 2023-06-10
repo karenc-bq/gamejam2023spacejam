a0230e0f71ef6a881a6f5ac099f79ff01fb35c99
Karen Chen <karenc@bitquilltech.com>
2023-06-10 10:27:33 -0700

    draw rooms

---
+++ scenes/Util.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+# Util.choose(["one", "two"])   returns one or two
+static func choose(choices):
+	randomize()
+
+	var rand_index = randi() % choices.size()
+	return choices[rand_index]
+
+# the percent chance something happens
+static func chance(num):
+	randomize()
+
+	if randi() % 100 <= num: return true
+	else:                    return false
+
+# returns random int between low and high
+static func randi_range(low, high):
+	return floor(randf_range(low, high))
--- scenes/game_screen.gd
+++ scenes/game_screen.gd
@@ -3,18 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	print("_ready")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game_screen.tscn")
-
-
-func _on_quit_button_pressed():
-	get_tree().quit()
+	print("_process: ")
 
