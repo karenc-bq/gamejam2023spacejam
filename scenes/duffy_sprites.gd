45f4fd02221e8461631fd7180da89cb796431e51
Crystal Liang <crystal.liang@improving.com>
2023-06-10 17:38:46 -0700

    fix merge conflicts

--- scenes/duffy_sprites.gd
+++ scenes/duffy_sprites.gd
@@ -14,12 +14,7 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	tile_map = get_parent().get_node("TileMap")
 	var start_room = tile_map.rooms[0]
-<<<<<<< HEAD
-	position = tile_map.map_to_local(start_room.center)
-=======
 	position = tile_map.map_to_local(Vector2(start_room.center.x - 1, start_room.center.y - 1))
-	
->>>>>>> a6b714d (some furniture)
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -40,18 +35,14 @@ func _process(delta):
 	
 	var future_pos = position + velocity * delta
 	var tile = tile_map.get_cell_atlas_coords(0, tile_map.local_to_map(Vector2i(future_pos.x, future_pos.y)))
-<<<<<<< HEAD
-	if (tile == Map.Tiles.BOTTOM_DOOR || tile == Map.Tiles.TOP_DOOR || tile == Map.Tiles.CORRIDOR || tile == Map.Tiles.GROUND || tile == Map.Tiles.EXIT):
-=======
 	var furnitureTile = tile_map.get_cell_atlas_coords(1, tile_map.local_to_map(Vector2i(future_pos.x, future_pos.y)))
-	if (tile == Map.Tiles.BOTTOM_DOOR || tile == Map.Tiles.TOP_DOOR || tile == Map.Tiles.CORRIDOR || tile == Map.Tiles.GROUND):
+	if (tile == Map.Tiles.BOTTOM_DOOR || tile == Map.Tiles.TOP_DOOR || tile == Map.Tiles.CORRIDOR || tile == Map.Tiles.GROUND || tile == Map.Tiles.EXIT):
 		var isFurniture = false
 		for i in range(Map.Furniture.FURNITURE_TILES.size()):
 			if furnitureTile == Map.Furniture.FURNITURE_TILES[i]:
 				isFurniture = true
 				break
 				
->>>>>>> a6b714d (some furniture)
 		# able to walk on the tile
 		if !isFurniture:
 			position = future_pos
