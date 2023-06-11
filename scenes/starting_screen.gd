4c96dc0e3718cf91542136d637734ec90797159f
Karen Chen <karenc@bitquilltech.com>
2023-06-11 12:06:54 -0700

    add blurb

--- scenes/starting_screen.gd
+++ scenes/starting_screen.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	get_node("blurb").hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game_screen.tscn")
-
 
 func _on_quit_button_pressed():
 	get_tree().quit()
 
+func _on_btn_mouse_entered():
+	get_node("blurb").show()
+
+func _on_btn_mouse_exited():
+	get_node("blurb").hide()
